f0b2fa221b4cb7f1101603f303f441e6e67de85e
dai kikuchi <kikuchi.dai@gmail.com>
2017-11-18 13:25:11 +1300

    first commit

---
+++ src/utils/api.js
@@ -0,0 +1,10 @@
+const API_ID = 'your ID'
+const APP_KEY = 'your Key'
+
+export function fetchRecipes (food = '') {
+  food = food.trim()
+
+  return fetch(`https://api.edamam.com/search?q=${food}&app_id=${API_ID}&app_key=${APP_KEY}`)
+    .then((res) => res.json())
+    .then(({ hits }) => hits.map(({ recipe }) => recipe))
+}
